6303ba97e28bd27bcac382b0b9b49bcc0a7e545d
David <rea12344@hotmail.com>
2025-02-04 21:39:00 -0800

    Se añadio arreglo y función

--- challenge-amigo-secreto_esp-main/challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,12 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//lista de amigos ingresados
+let amigos = ['',];
+console.log(amigos);
+
+//agregar amigos al pulsar botón añadir
+function agregarAmigo(){
+let input = document.getElementById('amigo').value;
+console.log(input)
+
+
+}
