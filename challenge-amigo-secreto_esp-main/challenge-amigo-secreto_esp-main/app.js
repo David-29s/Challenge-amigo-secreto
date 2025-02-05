50af29b9b78d86b4a614c6fd85c8d06c41e1763d
David <rea12344@hotmail.com>
2025-02-05 15:37:41 -0800

    se agrego la validación del input e implemento la lista de amigos y se limpio la caja al añadir amigos

--- challenge-amigo-secreto_esp-main/challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/challenge-amigo-secreto_esp-main/app.js
@@ -1,12 +1,24 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 //lista de amigos ingresados
-let amigos = ['',];
-console.log(amigos);
+let amigos = [];
 
 //agregar amigos al pulsar botón añadir
 function agregarAmigo(){
-let input = document.getElementById('amigo').value;
-console.log(input)
+let input = document.getElementById("amigo").value;
+        // Si está vacío o solo tiene espacios
+    if (input.trim() === "") {
+        alert("Por favor, inserte un nombre.");
+    }
+else{
+    amigos.push(input);
+    console.log(amigos);
+    // console.log(amigos[amigos.length - 1]); para mirar la ultima persona añadida.
+
+
+
+    }
+//Esto limpia el texto del input despues de añadirlo a la lista.
+document.getElementById("amigo").value = '';
 
 
 }
