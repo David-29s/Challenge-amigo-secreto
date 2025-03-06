e9263e15873bd09e8a39c961e43662755d1a6b55
David <rea12344@hotmail.com>
2025-03-05 17:14:26 -0800

    Challenge terminado

--- challenge-amigo-secreto_esp-main/challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/challenge-amigo-secreto_esp-main/app.js
@@ -1,24 +1,52 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 //lista de amigos ingresados
+
+
 let amigos = [];
+let lista = document.getElementById('listaAmigos');
 
 //agregar amigos al pulsar botón añadir
 function agregarAmigo(){
-let input = document.getElementById("amigo").value;
-        // Si está vacío o solo tiene espacios
-    if (input.trim() === "") {
+let input = document.getElementById("amigo").value.trim();
+        //validación.
+    if (input === "") {
         alert("Por favor, inserte un nombre.");
+        return;
     }
-else{
-    amigos.push(input);
+    // Verificar si el amigo ya está en la lista
+    if (amigos.includes(input)) {
+        alert("Este amigo ya está en la lista.");
+        return;
+    }
+
+    amigos.push(input); //Agrega el amigo a la lista
     console.log(amigos);
     // console.log(amigos[amigos.length - 1]); para mirar la ultima persona añadida.
 
-
-
-    }
-//Esto limpia el texto del input despues de añadirlo a la lista.
+    actualizarLista();
+//limpia el texto del input después de añadirlo a la lista.
 document.getElementById("amigo").value = '';
+document.getElementById("amigo").focus();
 
+}
+
+function actualizarLista(){
+    lista.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
+function sortearAmigo(){
+    if(amigos.length === 0){
+        alert("No hay amigos para sortear");
+        return;
+    }
+        let amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+        let resultado = document.getElementById("resultado");
+        resultado.innerHTML = `El amigo sorteado es: ${amigoSorteado}`;
+        let limpiarLista = document.getElementById("listaAmigos");
+        limpiarLista.innerHTML = '';
 
 }
