4fe611c0468cff4e04340a6ac3452ac1cda4653d
David <rea12344@hotmail.com>
2025-03-06 21:10:20 -0800

    Update app.js

--- challenge-amigo-secreto_esp-main/challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/challenge-amigo-secreto_esp-main/app.js
@@ -1,7 +1,4 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-//lista de amigos ingresados
-
-
 let amigos = [];
 let lista = document.getElementById('listaAmigos');
 
